a3e07967cc20a029859ce5b122ad6e3b4da62986
lucasG505 <lucas.mariano.g@gmail.com>
2023-05-09 12:32:55 -0300

    first user model

---
+++ back/src/models/User.js
@@ -0,0 +1,43 @@
+const {DataTypes}=require('sequelize');
+
+module.exports=(sequelize) => {
+    sequelize.define("user", {
+        id:{
+            type:DataTypes.UUID,
+            primaryKey: true,
+            defaultValue: DataTypes.UUIDV4
+        },
+        fullName: {
+            type:DataTypes.STRING,
+            allowNull:false,
+            validate:{
+                isAlpha:true
+            }
+        },
+        password: {
+            type:DataTypes.STRING,
+            allowNull:false,
+            validate:{
+                is: ["^(?=.*[0-9])(?=.*[A-Z])(?=.*[*.!@$%^&(){}[]:;<>,.?/~_+-=|\]).{6,32}$","i"]
+            }
+        },
+        email: {
+            type:DataTypes.STRING,
+            allowNull:false,
+            validate:{
+                isEmail:true
+            }
+        },
+        phone:{
+            type:DataTypes.NUMBER,
+            allowNull:false,
+            validate:{
+                isNumeric:true
+            }
+        },
+        isMonitor:{
+            type:DataTypes.BOOLEAN,
+            defaultValue:false
+        }
+    },{timestamps:false}) 
+}
