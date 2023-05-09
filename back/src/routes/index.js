7527392e38739cbc5e75d6d7991603b7a3c2f7d1
lucasG505 <lucas.mariano.g@gmail.com>
2023-05-09 16:03:23 -0300

    Saves

--- back/src/routes/index.js
+++ back/src/routes/index.js
@@ -2,11 +2,11 @@ const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 const users=require('./users');
-
+const lessons=require('./lessons');
 
 const router = Router();
 
 router.use('/users', users)
-
+router.use('/lessons', lessons)
 
 module.exports = router;
