b5e0ab69c5f7fbc0135bed737602ac7786662eb1
lucasG505 <lucas.mariano.g@gmail.com>
2023-05-09 17:02:33 -0300

    routes user & lessons

--- back/src/routes/index.js
+++ back/src/routes/index.js
@@ -1,1 +1,10 @@
-hola
+const { Router } = require('express');
+const lessons = require('./lessons');
+const users= require('./users');
+
+const router=Router();
+
+router.use('/lessons', lessons);
+router.use('/users', users);
+
+module.exports=router;
