948366035c392824bd871de7747fd8394e3ac186
JPGomezCuervo <122913492+JPGomezCuervo@users.noreply.github.com>
2023-05-09 14:55:17 -0500

    Fix size in the Gallery component

---
+++ client/src/components/Gallery/Gallery.jsx
@@ -0,0 +1,14 @@
+import style from './Gallery.module.css';
+const Gallery = () => {
+    return (
+        <div className={style.DivContainer}>
+
+            <div className={style.ImageContainer}>
+
+            </div>
+        </div>
+                
+
+    )
+}
+export default Gallery;
