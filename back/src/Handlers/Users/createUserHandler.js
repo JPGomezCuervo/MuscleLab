71809a8886ac09688b9924f851134b7377ea8e9a
GastonComparin <gastoncomparin92@gmail.com>
2023-05-10 20:37:20 -0300

    routes for lesson and users °encrypting user password

--- back/src/Handlers/Users/createUserHandler.js
+++ back/src/Handlers/Users/createUserHandler.js
@@ -1,20 +1,22 @@
 const createUser = require("../../Controllers/Users/createUser");
-
-
+const bcryptjs = require("bcryptjs");
 const createNewUser = async (req, res) => {
-  const { id, fullName, password, email, phone, isMonitor } = req.body;
+  const { id, fullName, email, phone, isMonitor } = req.body;
+  const password = req.body.password;
   try {
+    let passwordHash = await bcryptjs.hash(password, 8);
     const newUser = await createUser(
       id,
       fullName,
-      password,
+      passwordHash,
       email,
       phone,
       isMonitor
     );
-    res
-      .status(201)
-      .json({ message: "User created succesfully", user: newUser });
+    res.status(201).json({
+      message: "User created succesfully",
+      user: newUser,
+    });
   } catch (error) {
     res.status(400).json(error.message);
   }
