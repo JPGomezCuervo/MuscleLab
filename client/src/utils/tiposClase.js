8070db90c50db0727f73bd428c2e1ed103b2976a
JPGomezCuervo <122913492+JPGomezCuervo@users.noreply.github.com>
2023-05-10 16:57:17 -0500

    Add a utils files about types of exercises

---
+++ client/src/utils/tiposClase.js
@@ -0,0 +1,15 @@
+const tiposClase = [
+    'Fuerza',
+    'Cardio',
+    'Flexibilidad',
+    'Equilibrio',
+    'Coordinación',
+    'Velocidad',
+    'Resistencia',
+    'Agilidad',
+    'Potencia',
+    'Movilidad',
+    'Estiramiento',
+    'Relajación',
+
+];
