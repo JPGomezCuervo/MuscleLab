80ddc281c73b8d7e5e1e4ee4f7fb369f3b7d3f46
lucasG505 <lucas.mariano.g@gmail.com>
2023-05-11 10:55:47 -0300

    Create detail

--- back/src/Controllers/Lessons/createLesson.js
+++ back/src/Controllers/Lessons/createLesson.js
@@ -1,15 +1,27 @@
-const { Lessons } = require("../../db");
+const { Lessons, LessonDetail } = require("../../db");
 
-let createLesson = async (id, name) => {
+let createLesson = async (id, name, effort, goals, description, scheduleDays, scheduleHours) => {
+  console.log(effort, name, goals, description, scheduleDays, scheduleHours);
   const foundedClass = await Lessons.findOne({ where: { name: name } });
   if (foundedClass) {
     throw new Error("That class already exist");
   } else {
+    if(!effort || !goals || !name  || !description || !scheduleDays || !scheduleHours){
+      throw new Error("Missing data");
+    }
     const newLesson = await Lessons.create({
       id,
       name,
     });
-    return `id: ${newLesson.id} name: ${newLesson.name}`;
+    const details = await LessonDetail.create({
+      id,
+      effort,
+      goals,
+      description,
+      scheduleDays,
+      scheduleHours
+    });
+    return `id: ${newLesson.id} name: ${newLesson.name} details: ${details}`;
   }
 };
 module.exports = createLesson;
