0409c3e2909ef47fc8d08b79e250c1f4016c4825
JPGomezCuervo <122913492+JPGomezCuervo@users.noreply.github.com>
2023-05-11 11:53:33 -0500

    Correct footer height

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -4,11 +4,23 @@ const Footer = () =>{
     return(
         <footer className={style.Footer}>
             <div className={style.InfoContainer}>
-                <div className={style.Info}>texto</div>
-                <div className={style.Info}>texto</div>
-                <div className={style.Info}>texto</div>
-                <div className={style.Info}>texto</div>
-                <div className={style.Info}>texto</div>
+                <div className={style.Container}>
+                    <div className={style.Info}>
+                        <h3>Información</   h3>
+                    </div>
+                    <div className={style.Info}>
+                        <h3>Navegación</h3>
+                    </div>
+                    <div className={style.Info}>
+                        <h3>Nosotros</h3>
+                    </div>
+                    <div className={style.Info}>
+                        <h3>Sedes</h3>
+                    </div>
+                    <div className={style.Info}>
+                        <h3>Redes Sociales</h3>
+                    </div>
+                </div>
             </div>
         </footer>
     )
