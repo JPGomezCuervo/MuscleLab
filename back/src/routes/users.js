b51e7824d491c312f452d23811053d6c887abd25
GastonComparin <gastoncomparin92@gmail.com>
2023-05-11 00:10:57 -0300

    login user route done

--- back/src/routes/users.js
+++ back/src/routes/users.js
@@ -4,12 +4,12 @@ const server = Router();
 const createNewUser = require("../Handlers/Users/createUserHandler");
 const deleteMyUser = require("../Handlers/Users/deleteUserHandler");
 const getAllUsers = require("../Handlers/Users/getAllUsersHandler");
-
+const loginUser = require("../Handlers/Users/loginUserHandler");
 server.get("/:id", async (req, res) => {
   res.status(200);
 });
 server.get("/", getAllUsers);
 server.post("/create", createNewUser);
 server.delete("/delete/:id", deleteMyUser);
-
+server.post("/login", loginUser);
 module.exports = server;
