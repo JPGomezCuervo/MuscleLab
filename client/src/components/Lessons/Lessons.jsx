fbe69a0ad03d915490c019b75add2353df45dfed
JPGomezCuervo <122913492+JPGomezCuervo@users.noreply.github.com>
2023-05-11 12:16:57 -0500

    fix hovers home and navbar

--- client/src/components/Lessons/Lessons.jsx
+++ client/src/components/Lessons/Lessons.jsx
@@ -1,6 +1,7 @@
 import style from './Lessons.module.css';
 import { useSelector, useDispatch } from 'react-redux';
 import { selectAllLessons } from '../../redux/features/lessonsSlice';
+import { useEffect } from 'react';
 import FilterBar from '../FilterBar/FilterBar';
 import Lesson from '../Lesson/Lesson';
 import lessons from '../../utils/lessons';
@@ -8,6 +9,9 @@ import tiposClase from '../../utils/tiposClase';
 const Lessons = () => {
     //cuando se funcionen las rutas del server descomenta esto:
 //   const lessons = useSelector(selectAllLessons);
+    useEffect(() => {
+        window.scrollTo({top: 0, left:0, behavior: 'auto'})
+    }, []);
     
     return (
             <div className={style.Frame}>  
