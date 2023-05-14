b1f9d3879b4821e3a7121c84281eba86debf5a41
GastonComparin <gastoncomparin92@gmail.com>
2023-05-14 17:06:23 -0300

    feature: CreateLesson component done -se agregó un boton en navbar para ir al componente crear -se creó el formulario, con validaciones en name, effort, y horarios

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,30 @@
-import './App.css';
-import { Route, Routes } from 'react-router-dom';
-import NavBar from './components/NavBar/NavBar';
-import Home from './components/Home/Home';
-import Lessons from './components/Lessons/Lessons';
-import Footer from './components/Footer/Footer';
-import DetailLesson from './components/DetailLesson/DetailLesson';
-import ContactUs from './components/ContactUs/ContactUs'
-import Login from './components/Login/Login';
-import SignUp from './components/SignUp/SignUp';
-import NewLessons from './components/NewLessons/NewLessons';
-
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import NavBar from "./components/NavBar/NavBar";
+import Home from "./components/Home/Home";
+import Lessons from "./components/Lessons/Lessons";
+import Footer from "./components/Footer/Footer";
+import DetailLesson from "./components/DetailLesson/DetailLesson";
+import ContactUs from "./components/ContactUs/ContactUs";
+import Login from "./components/Login/Login";
+import SignUp from "./components/SignUp/SignUp";
+import NewLessons from "./components/NewLessons/NewLessons";
+import CreateLesson from "./components/CreateLesson/CreateLesson";
 function App() {
   return (
-   <>
-    <NavBar/>
-    <Routes>
-      <Route path='/login' element={<Login/>}/>
-      <Route path="/" element={<Home/>}/>
-      <Route path='/clases' element={<Lessons/>}/>
-      <Route path= '/detail' element={<DetailLesson/>}/>
-      <Route path= '/contactanos' element={<ContactUs />}/>
-      <Route path= '/clases/detalles' element={<DetailLesson/>}/>
-    </Routes>
-    <Footer/>
-  </> 
+    <>
+      <NavBar />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/clases" element={<Lessons />} />
+        <Route path="/detail" element={<DetailLesson />} />
+        <Route path="/contactanos" element={<ContactUs />} />
+        <Route path="/clases/detalles" element={<DetailLesson />} />
+        <Route path="/create" element={<CreateLesson />} />
+      </Routes>
+      <Footer />
+    </>
   );
 }
 
--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,58 +1,66 @@
-import React from 'react';
-import style from './NavBar.module.css';
-import iconMan from './../../assets/icons/man-silhouette.png';
-import iconWeight from '../../assets/icons/dumbbell.png';
-import { Link } from 'react-router-dom';
-import SignUp from '../SignUp/SignUp';
+import React from "react";
+import style from "./NavBar.module.css";
+import iconMan from "./../../assets/icons/man-silhouette.png";
+import iconWeight from "../../assets/icons/dumbbell.png";
+import { Link } from "react-router-dom";
+import SignUp from "../SignUp/SignUp";
 
-const NavBar = ()=>{
+const NavBar = () => {
+  const handleClickLogo = () => {
+    window.location.href = "/";
+  };
 
-    const handleClickLogo = () => {
-        window.location.href = '/';
-    };
+  return (
+    <>
+      <nav className={style.nav}>
+        <div className={style.IconContainer} onClick={handleClickLogo}>
+          <img
+            className={style.IconMan}
+            src={iconMan}
+            alt="Icono silueta de hombre"
+          />
+          <h2>Muscle Lab</h2>
+          <img
+            className={style.IconWeight}
+            src={iconWeight}
+            alt="Icono de pesa"
+          />
+        </div>
+        <div className={style.OpcionsContainer}>
+          <Link to={"/sedes"}>
+            <p className={style.Navbutton}>Sedes</p>
+          </Link>
 
-     return (
-        <>
-        <nav className={style.nav}>
-            <div className={style.IconContainer} onClick={handleClickLogo}>
-                <img className={style.IconMan} src={iconMan} alt='Icono silueta de hombre' />
-                <h2>Muscle Lab</h2>
-                <img className={style.IconWeight} src={iconWeight} alt='Icono de pesa' />               
-            </div>
-            <div className={style.OpcionsContainer}>
-                <Link to = {'/sedes'}>
-                    <p className={style.Navbutton}>Sedes</p>
-                </Link>
+          <Link to={"/clases"}>
+            <p className={style.Navbutton}>Clases</p>
+          </Link>
 
-                <Link to = {'/clases'}>
-                    <p className={style.Navbutton}>Clases</p>
-                </Link>
+          <Link to={"/nosotros"}>
+            <p className={style.Navbutton}>Nosotros</p>
+          </Link>
 
-                <Link to = {'/nosotros'}>
-                    <p className={style.Navbutton}>Nosotros</p>
-                </Link>
+          <Link to={"/contactanos"}>
+            <p className={style.Navbutton}>Contáctanos</p>
+          </Link>
+        </div>
+        <button className={style.btnPlanes}>Planes</button>
 
-                <Link to = {'/contactanos'}>
-                    <p className={style.Navbutton}>Contáctanos</p>
-                </Link>
+        <div className={style.LogOpcions}>
+          <Link to={"/create"}>
+            <button className={style.btnInicioSesion}>Crear Clase</button>
+          </Link>
 
-            </div>
-            <button className={style.btnPlanes}>Planes</button>
+          <Link to={"/login"}>
+            <button className={style.btnInicioSesion}>Inicia sesión</button>
+          </Link>
 
-            <div className={style.LogOpcions}>
-                <Link to={'/login'}> 
-                    <button className={style.btnInicioSesion}>Inicia sesión</button>
-                </Link> 
-                <Link to={SignUp}>
-                    <button className={style.btnRegistro}>Regístrate</button>
-                </Link>
-            </div>
-            
-        </nav>
-        
-        </>
-     )
-}
-
+          <Link to={SignUp}>
+            <button className={style.btnRegistro}>Regístrate</button>
+          </Link>
+        </div>
+      </nav>
+    </>
+  );
+};
 
 export default NavBar;
