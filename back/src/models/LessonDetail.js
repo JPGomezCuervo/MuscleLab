5530441d389f4b330e8138b25fca203ad076fc2a
GastonComparin <gastoncomparin92@gmail.com>
2023-05-15 20:52:01 -0300

    uuid

--- back/src/models/LessonDetail.js
+++ back/src/models/LessonDetail.js
@@ -1,32 +1,41 @@
-const { DataTypes } = require('sequelize')
+const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
-    sequelize.define('lessonDetail', {
-        id: {
-            type: DataTypes.UUID,
-            defaultValue: DataTypes.UUIDV4,
-            primaryKey:true,
-            allowNull: false
-        },
-        goals: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        description: {
-            type: DataTypes.TEXT,
-            allowNull: false
-        },
-        scheduleDays: {
-            type: DataTypes.ARRAY(DataTypes.STRING),
-            allowNull:false
-        },
-        scheduleHourStart: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        scheduleHourFinish: {
-            type: DataTypes.STRING,
-            allowNull: false
-        }
-    }, { timestamps: false })
-}
+  sequelize.define(
+    "lessonDetail",
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+        allowNull: false,
+      },
+      goals: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      scheduleDays: {
+        type: DataTypes.ARRAY(DataTypes.STRING),
+        allowNull: false,
+      },
+      scheduleHourStart: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      scheduleHourFinish: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      lessonId: {
+        type: DataTypes.UUID,
+        allowNull: false,
+        foreignKey: true,
+      },
+    },
+    { timestamps: false }
+  );
+};
