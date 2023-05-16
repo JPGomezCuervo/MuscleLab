d907c808fe3da1743f8e84b546686be7884c7e69
GastonComparin <gastoncomparin92@gmail.com>
2023-05-16 18:35:36 -0300

    feat: getBranchoffice route added

--- back/src/routes/branchoffice.js
+++ back/src/routes/branchoffice.js
@@ -1,7 +1,9 @@
 const { Router } = require("express");
 const server = Router();
 const createNewBranchoffice = require("../Handlers/Branchoffice/createBranchofficeHandler");
-const getAllBranchoffice = require("../Handlers/Branchoffice/getBranchofficeHandler")
+const getAllBranchoffice = require("../Handlers/Branchoffice/getBranchofficeHandler");
+const getBranchofficeDetail = require("../Handlers/Branchoffice/getBranchofficeDetailHandler");
 
 server.post("/create", createNewBranchoffice);
 server.get("/", getAllBranchoffice);
+server.get("/:id", getBranchofficeDetail);
