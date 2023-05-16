dc7673452674442fb29b9f197aaad2f32f0700c0
JPGomezCuervo <122913492+JPGomezCuervo@users.noreply.github.com>
2023-05-16 17:30:57 -0500

    add new routes

---
+++ client/src/components/LesssonsDash/LessonsDash.jsx
@@ -0,0 +1,10 @@
+import style from "./LessonsDash.module.css";
+
+const LessonsDash = () => {
+    return(
+        <div className= {style.Container}>
+        <h1>hola</h1>
+        </div>
+    )
+}
+export default LessonsDash
