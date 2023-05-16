8296f0c1130bf1b972445d283c6c0a78543e2a20
GastonComparin <gastoncomparin92@gmail.com>
2023-05-16 18:58:14 -0300

    feat: updateBranchoffice route added

--- back/src/routes/branchoffice.js
+++ back/src/routes/branchoffice.js
@@ -3,9 +3,15 @@ const server = Router();
 const createNewBranchoffice = require("../Handlers/Branchoffice/createBranchofficeHandler");
 const getAllBranchoffice = require("../Handlers/Branchoffice/getBranchofficeHandler");
 const getBranchofficeDetail = require("../Handlers/Branchoffice/getBranchofficeDetailHandler");
-const deleteMyBranchoffice = requiere("../Handlers/Branchoffice/deleteBranchofficeHandler")
+const deleteMyBranchoffice = requiere(
+  "../Handlers/Branchoffice/deleteBranchofficeHandler"
+);
+const updateMyBranchoffice = requiere(
+  "../Handlers/Branchoffice/updateBranchofficeHandler"
+);
 
 server.post("/create", createNewBranchoffice);
 server.get("/", getAllBranchoffice);
 server.get("/:id", getBranchofficeDetail);
 server.delete("/delete/:id", deleteMyBranchoffice);
+server.put("/update/:id", updateMyBranchoffice);
