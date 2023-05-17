af2ae8c8d4a4f6e8b1421bb9d2e62fb35a15ca99
GastonComparin <gastoncomparin92@gmail.com>
2023-05-16 23:56:49 -0300

    allow null :true

--- back/src/models/LessonDetail.js
+++ back/src/models/LessonDetail.js
@@ -37,7 +37,7 @@ module.exports = (sequelize) => {
       },
       shortDescription:{
         type:DataTypes.STRING,
-        allowNull:false
+        allowNull:true
     }
     },
     { timestamps: false }
