c32e4fd8d610ec5d5b06e9cd2c072c0b123610fe
lucasG505 <lucas.mariano.g@gmail.com>
2023-05-18 14:47:34 -0300

    deploy

--- back/src/db.js
+++ back/src/db.js
@@ -4,10 +4,16 @@ const fs = require("fs");
 const path = require("path");
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DATABASE_URL } = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
+const sequelize = new Sequelize(`${DATABASE_URL}`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-  dialect: 'postgres'
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false // En entornos de producción, debes configurar esto en `true` y proporcionar el certificado adecuado
+    }
+  }
 });
 const basename = path.basename(__filename);
 
