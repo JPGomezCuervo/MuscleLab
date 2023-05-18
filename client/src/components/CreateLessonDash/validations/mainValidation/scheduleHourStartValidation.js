dc98d624c0e51b7ba4e3313d69f3a78087be30eb
JPGomezCuervo <122913492+JPGomezCuervo@users.noreply.github.com>
2023-05-18 16:34:45 -0500

    Implement Edit/create component

--- client/src/components/CreateLessonDash/validations/mainValidation/scheduleHourStartValidation.js
+++ client/src/components/CreateLessonDash/validations/mainValidation/scheduleHourStartValidation.js
@@ -1,5 +1,8 @@
-const scheduleHourStartValidation = (value, errors, name) => {
-    if(value === 0 ) return {...errors, [name]: 'La hora de inicio es requerida'};
+const scheduleHourStartValidation = (value, errors, name, lessonAttributes) => {
+
+    if (value === 0 ) return {...errors, [name]: 'La hora de inicio es requerida'};
+    if (value === lessonAttributes.scheduleHourFinish ) return {...errors, [name]: 'La hora de inicio no puede ser igual a la hora de finalización'};
+
     return {...errors, [name]: ''};
 }
 
--- client/src/components/TrainerDash/TrainerDash.jsx
+++ client/src/components/TrainerDash/TrainerDash.jsx
@@ -17,19 +17,37 @@ const TrainerDash = () => {
                 <h2>Clases</h2>
             </div>
 
-            <div className={style.StatusBar}>
-                <span> Nombre </span>
-                <span> Clases </span>
-                <span> Status </span>
-            </div>
-
-            <div className={style.TeachersContainer}>
+            <div className={style.Container}>
                 <div className={style.NameContainer}>
-                    
+                    <h2 className={`${style.Title} ${style.Info}`}> Nombre</h2>
+
+                    <h2 className={style.InfoName}> Lorem ipsum dolor</h2>
+                    <h2 className={style.InfoName}> Lorem ipsum dolor</h2>
+                    <h2 className={style.InfoName}> Lorem ipsum dolor</h2>
+                    <h2 className={style.InfoName}> Lorem ipsum dolor</h2>
+                    <h2 className={style.InfoName}> Lorem ipsum dolor</h2>
+                    <h2 className={style.InfoName}> Lorem ipsum dolor</h2>
                 </div>
 
-                <div className={style.Lessons}>
-                    
+                <div className={style.TrainerContainer}>
+                    <h2 className={`${style.Title} ${style.Info}`}> Profesor</h2>
+                    <h2 className={style.Info}> Brad Pitt</h2>
+                    <h2 className={style.Info}> Antonio Gomez</h2>
+                    <h2 className={style.Info}> Brad Pitt</h2>
+                    <h2 className={style.Info}> Brad Pitt</h2>
+                    <h2 className={style.Info}> Antonio Gomez</h2>
+                    <h2 className={style.Info}> Antonio Gomez</h2>
+                </div>
+
+                <div className={style.StatusContainer}>
+                    <h2 className={`${style.Title} ${style.Info}`}> Nombre</h2>
+
+                    <h2 className={style.Info}> Activo</h2>
+                    <h2 className={style.Info}> Inactivo</h2>
+                    <h2 className={style.Info}> Inactivo</h2>
+                    <h2 className={style.Info}> Activo</h2>
+                    <h2 className={style.Info}> Activo</h2>
+                    <h2 className={style.Info}> Inactivo</h2>
                 </div>
             </div>
 
