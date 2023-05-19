d3b43719cb075f4e0e2860ff1f5ba733ca03e7d3
lucasG505 <lucas.mariano.g@gmail.com>
2023-05-19 17:03:01 -0300

    isAvailable no necesrio en update detail

--- back/src/Controllers/Lessons/updateDetailLessonsController.js
+++ back/src/Controllers/Lessons/updateDetailLessonsController.js
@@ -1,39 +1,39 @@
 const { LessonDetail } = require('../../db');
 
-const updateLessonDetail = async( 
-    id,  
+const updateLessonDetail = async (
+    id,
     description,
-    name, 
-    scheduleDays , 
-    scheduleHourStart, 
+    name,
+    scheduleDays,
+    scheduleHourStart,
     scheduleHourFinish,
     isAvailable) => {
     const foundedDetail = await LessonDetail.findOne({
-        where:{
-            id:id
+        where: {
+            id: id
         }
-    }); 
+    });
     if (!foundedDetail) {
         throw new Error('El detalle de la clase que quieres modificar no existe!');
-    } 
-        if(
-            !name ||
-            !description  || 
-            !scheduleDays || 
-            !scheduleHourStart || 
-            !scheduleHourFinish || !isAvailable){
-                throw new Error('Todos los campos son obligatorios');
-        }
-        await foundedDetail.update({
-            name: name,
-            description: description,
-            scheduleDays: scheduleDays,
-            scheduleHourStart: scheduleHourStart,
-            scheduleHourFinish: scheduleHourFinish,
-            isAvailable: isAvailable
-        });
-        
-        return ("Datos actualizados correctamente");
+    }
+    if (
+        !name ||
+        !description ||
+        !scheduleDays ||
+        !scheduleHourStart ||
+        !scheduleHourFinish) {
+        throw new Error('Todos los campos son obligatorios');
+    }
+    await foundedDetail.update({
+        name: name,
+        description: description,
+        scheduleDays: scheduleDays,
+        scheduleHourStart: scheduleHourStart,
+        scheduleHourFinish: scheduleHourFinish,
+        isAvailable: isAvailable
+    });
+
+    return ("Datos actualizados correctamente");
 };
 
 module.exports = updateLessonDetail;
