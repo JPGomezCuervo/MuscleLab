b165c56b36660a82535a9bc940b9ca2c6868ecec
GastonComparin <gastoncomparin92@gmail.com>
2023-05-18 22:59:22 -0300

    force db

--- back/index.js
+++ back/index.js
@@ -23,7 +23,7 @@ const { conn } = require("./src/db.js");
 // Syncing all the models at once.
 
 conn
-  .sync({ alter: true })
+  .sync({ force: true })
   .then(() => {
     server.listen(5432, () => {
       console.log("Server listening on port 5432"); // eslint-disable-line no-console
