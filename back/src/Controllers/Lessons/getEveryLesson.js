fd355f49642c395e07a0ad0fcbf00f7941058c60
lucasG505 <lucas.mariano.g@gmail.com>
2023-05-20 15:05:17 -0300

    short description en traer todas las lessons

--- back/src/Controllers/Lessons/getEveryLesson.js
+++ back/src/Controllers/Lessons/getEveryLesson.js
@@ -3,12 +3,34 @@ const { Op } = require("sequelize");
 const {getDetailLesson}= require('./getDetail');
 
 const getEveryLesson =async ()=>{
-    const lessonsRaw=await Lessons.findAll({attributtes:['name']});
+    const lessonsRaw=await Lessons.findAll();
     const lessonsName= lessonsRaw.map(el=>el.name);
     const everyLesson= [];
     for(let i=0;i<lessonsName.length;i++){
         let some=await getDetailLesson(lessonsName[i]);
-        some.map(el=>everyLesson.push(el));
+        some.map(el=>{
+            let shortDescription=lessonsRaw.map(el=>{return{shortDescription:el.shortDescription, name:el.name}});
+            for(let i=0;i<shortDescription.length;i++){
+                if(el.name.includes(shortDescription[i].name)){
+                    const objDetail = {
+                        id: el.id,
+                        name: el.name,
+                        image: el.image,
+                        effort: el.effort,
+                        goals: el.goals,
+                        shortDescription: shortDescription[i].shortDescription,
+                        scheduleDays: el.scheduleDays,
+                        scheduleHourStart: el.scheduleHourStart,
+                        scheduleHourFinish: el.scheduleHourFinish,
+                        isAvailable:el.isAvailable,
+                        types: el.types,
+                        monitors: el.monitors,
+                        office: el.office
+                      }
+                    everyLesson.push(objDetail)
+                }
+            }
+        });
     }
     return everyLesson;
 }
