1c5a2372128471258aba612f0d89e615b28e02d6
GastonComparin <gastoncomparin92@gmail.com>
2023-05-21 22:28:01 -0300

    prueba2

--- back/src/routes/branchoffice.js
+++ back/src/routes/branchoffice.js
@@ -1,11 +1,11 @@
 const { Router } = require("express");
 const server = Router();
 const Lalo = require("../Handlers/Branchoffice/prueba")
-const createNewBranchOffice = require("../Handlers/Branchoffice/createBranchofficeHandler.js");
-const getAllBranchOffice = require("../Handlers/Branchoffice/getBranchofficeHandler.js");
-const getBranchOfficeDetail = require("../Handlers/Branchoffice/getBranchofficeDetailHandler.js");
-const deleteMyBranchOffice = require("../Handlers/Branchoffice/deleteBranchofficeHandler.js");
-const updateMyBranchOffice = require("../Handlers/Branchoffice/updateBranchofficeHandler.js");
+const createNewBranchOffice = require("../Handlers/Prueba/createBranchofficeHandler");
+const getAllBranchOffice = require("../Handlers/Prueba/getBranchofficeHandler.js");
+const getBranchOfficeDetail = require("../Handlers/Prueba/getBranchofficeDetailHandler.js");
+const deleteMyBranchOffice = require("../Handlers/Prueba/deleteBranchofficeHandler.js");
+const updateMyBranchOffice = require("../Handlers/Prueba/updateBranchofficeHandler.js");
 server.post("/create", createNewBranchOffice);
 server.get("/", getAllBranchOffice);
 server.get("/:id", getBranchOfficeDetail);
