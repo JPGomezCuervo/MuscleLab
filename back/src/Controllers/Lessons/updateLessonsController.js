53215a4a7d7216941de04b87cd4e066bb7108cc8
lucasG505 <lucas.mariano.g@gmail.com>
2023-05-22 16:48:20 -0300

    updatea el monitor

--- back/src/Controllers/Lessons/updateLessonsController.js
+++ back/src/Controllers/Lessons/updateLessonsController.js
@@ -1,21 +1,29 @@
-const { Lessons, LessonDetail } = require("../../db");
+const { Lessons, LessonDetail, User } = require("../../db");
 const {Op}= require("sequelize");
 
-const updateLesson = async (id, effort, shortDescription, image, goals, name, description, scheduleDays, scheduleHourStart, scheduleHourFinish, isAvailable) => {
+const updateLesson = async (id, effort, shortDescription, image, goals, name, description, scheduleDays, scheduleHourStart, scheduleHourFinish, isAvailable, monitor) => {
   const foundedDetail = await LessonDetail.findOne({
     where: {
       id: id,
     },
-  });
-  const checkName = await LessonDetail.findOne({
-    where:{
-      name:name,
-      id:{[Op.not]:id}
+    include:{
+      model:User,
+      where:{isMonitor:true}
     }
   });
   const foundedLesson = await Lessons.findOne({
     where:{
       id:foundedDetail.lessonId
+    }
+  });
+  const oldMon=foundedDetail.dataValues.users[0];
+  const newMon= await User.findOne({where:{fullName:monitor}});
+  // let oldTypes =await foundedLesson.getExercisesTypes();
+  // oldTypes=oldTypes.map(t=>t.name);
+  const checkName = await LessonDetail.findOne({
+    where:{
+      name:name,
+      id:{[Op.not]:id}
     }
   });
   if(checkName){
@@ -41,6 +49,17 @@ const updateLesson = async (id, effort, shortDescription, image, goals, name, de
     image:image,
     goals:goals
   })
+  if(oldMon.id!==newMon.id){
+    foundedDetail.removeUser(oldMon?.id);
+    foundedDetail.addUser(newMon?.id);
+  }
+  // if(oldTypes.sort().join('')!==types.sort().join('')){
+  //   foundedLesson.setExercisesTypes([]);
+  //   types.map(async (t)=>{
+  //     const type= await ExercisesType.findOne({attributes:['id'],where:{name:t}});
+  //     foundedLesson.addExercisesType(type?.id);
+  //   })
+  // }
   return "Datos actualizados correctamente.";
 };
 
--- back/src/Handlers/Lessons/updateLessonHandler.js
+++ back/src/Handlers/Lessons/updateLessonHandler.js
@@ -2,7 +2,7 @@ const updateLesson = require("../../Controllers/Lessons/updateLessonsController"
 const { LessonDetail } = require("../../db");
 const updateLessons = async (req, res) => {
   const { id } = req.params;
-  const { effort, shortDescription, image, goals, name, description, scheduleDays , scheduleHourStart, scheduleHourFinish, isAvailable } = req.body;
+  const { effort, shortDescription, image, goals, name, description, scheduleDays , scheduleHourStart, scheduleHourFinish, isAvailable, monitor } = req.body;
   try {
     const updatedLesson = await updateLesson(
       id,
@@ -15,7 +15,8 @@ const updateLessons = async (req, res) => {
       scheduleDays , 
       scheduleHourStart, 
       scheduleHourFinish,
-      isAvailable
+      isAvailable,
+      monitor
     );
     res.status(200).json(updatedLesson);
   } catch (error) {
