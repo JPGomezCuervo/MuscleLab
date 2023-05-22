d9a8dd19dbe441be784db72ee1138933e27f983e
GastonComparin <gastoncomparin92@gmail.com>
2023-05-22 12:23:40 -0300

    fix: isAdmin user property

--- back/src/Controllers/Users/createUser.js
+++ back/src/Controllers/Users/createUser.js
@@ -1,6 +1,14 @@
 const { User } = require("../../db");
 
-let createUser = async (id, fullName, password, email, phone, isMonitor) => {
+let createUser = async (
+  id,
+  fullName,
+  password,
+  email,
+  phone,
+  isMonitor,
+  isAdmin
+) => {
   const foundedUser = await User.findOne({ where: { email: email } });
   if (foundedUser) {
     throw new Error("That email has already been registered ");
@@ -13,6 +21,7 @@ let createUser = async (id, fullName, password, email, phone, isMonitor) => {
         email,
         phone,
         isMonitor,
+        isAdmin,
       });
 
       return newUser;
