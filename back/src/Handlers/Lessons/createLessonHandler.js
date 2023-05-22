1d2114b7a079afefbc12524a2ca9c0e1eed8c1ed
GastonComparin <gastoncomparin92@gmail.com>
2023-05-21 22:38:43 -0300

    log

--- back/src/Handlers/Lessons/createLessonHandler.js
+++ back/src/Handlers/Lessons/createLessonHandler.js
@@ -1,10 +1,38 @@
 const createLesson = require("../../Controllers/Lessons/createLesson");
 
 const createNewLesson = async (req, res) => {
-  const { id, name, effort, goals,shortDescription, description, scheduleDays, scheduleHourStart,scheduleHourFinish,image, types, monitor, branchOffice } = req.body;
-  
+  const {
+    id,
+    name,
+    effort,
+    goals,
+    shortDescription,
+    description,
+    scheduleDays,
+    scheduleHourStart,
+    scheduleHourFinish,
+    image,
+    types,
+    monitor,
+    branchOffice,
+  } = req.body;
+  console.log("esto es el req.body", req.body);
   try {
-    const newLesson = await createLesson(id, name, effort, goals,shortDescription, description, scheduleDays, scheduleHourStart,scheduleHourFinish, image, types, monitor, branchOffice);
+    const newLesson = await createLesson(
+      id,
+      name,
+      effort,
+      goals,
+      shortDescription,
+      description,
+      scheduleDays,
+      scheduleHourStart,
+      scheduleHourFinish,
+      image,
+      types,
+      monitor,
+      branchOffice
+    );
     res
       .status(201)
       .json({ message: "Lesson created succesfully", lesson: newLesson });
