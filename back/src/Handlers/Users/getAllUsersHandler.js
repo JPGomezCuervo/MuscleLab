7986bc445f32d3a5a39a95a2ebf80b585edbc248
lucasG505 <lucas.mariano.g@gmail.com>
2023-05-23 14:53:30 -0300

    Get users trae sus clases asociadas

--- back/src/Handlers/Users/getAllUsersHandler.js
+++ back/src/Handlers/Users/getAllUsersHandler.js
@@ -2,8 +2,6 @@ const getUsers = require("../../Controllers/Users/getUsers");
 
 const getAllUsers = async (req, res) => {
   try {
-    let usertoken = req.user;
-    console.log(usertoken);
     let user = await getUsers();
     res.status(200).json({ user: user });
   } catch (error) {
