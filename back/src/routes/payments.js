e8e9046b21ee24f96d42d721170b82af3fde4338
lucasG505 <lucas.mariano.g@gmail.com>
2023-05-25 18:34:07 -0300

    tutti

--- back/src/routes/payments.js
+++ back/src/routes/payments.js
@@ -1,20 +1,17 @@
 const { Router } = require("express");
 const server = Router();
-const Stripe = require('stripe');
-const stripe= new Stripe("sk_test_51NBkBJFPcDe3Fz6KjLlzjI35wfptX5dwAkeq7TnXdPy9YEBmqirmm4YdUIktaK82RmXCH8WU1dxyp6cR5G6CbMMM00zSOxlAwo");
+const stripe = require('stripe')('sk_test_51NBkBJFPcDe3Fz6KjLlzjI35wfptX5dwAkeq7TnXdPy9YEBmqirmm4YdUIktaK82RmXCH8WU1dxyp6cR5G6CbMMM00zSOxlAwo');
 
 
 
-server.post('/checkout',async (req,res)=>{
-    const {id, amount, description}=req.body;
-    const payment= await stripe.paymentIntents.create({
-        amount: amount,
-        currency: "USD",
-        description: description,
-        payment_method: id,
-        confirm: true
-    });
-    res.status(200).json(payment);
+server.post('/create_checkout',async (req,res)=>{
+    res.status(200).json({msg:"Hola"});
+    const session=await stripe.checkout.sessions.create({
+        line_items:[{
+
+        }]
+    })
+    
 });
 
 module.exports = server;
