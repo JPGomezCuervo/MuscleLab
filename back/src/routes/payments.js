8f0086157419478ce67cce7252180930c7bed220
lucasG505 <lucas.mariano.g@gmail.com>
2023-05-29 19:36:50 -0300

    validacion de usuario ya con membresia

--- back/src/routes/payments.js
+++ back/src/routes/payments.js
@@ -7,37 +7,42 @@ const {User, StatusMemberships}=require('../db');
 server.post('/create_checkout',async (req,res)=>{
     const {name, benefits, price, duration, id}=req.body;
     const user=await User.findOne({where:{id:id}});
-    if(!user){
-        throw new Error("No se encontro al usuario");
-    }
-    const existing=await StatusMemberships.findOne({where:{userId:id}});
-    if(existing){
-        throw new Error(`Ya tiene una membresia activa. Vence el ${existing.end}`);
-    }
-    //aca customer probablemente sea el token del usuario loggeado por lo que habria que usar jwt para decodificar y sacar su email
-    const customer = user.email;
-    const charge=Number(price)*100;
-    const description= benefits + ' ' + duration;
-    const session=await stripe.checkout.sessions.create({
-        //aca el valor de customer email deberia ser, efectivamente el email una vez sacado del token
-        customer_email:customer,
-        line_items:[{
-            price_data:{
-                currency: "USD",
-                product_data:{
-                    name:name,
-                    description:description,
+    try {
+        
+        if(!user){
+            throw new Error("No se encontro al usuario");
+        }
+        const existing=await StatusMemberships.findOne({where:{userId:id}});
+        if(existing){
+            throw new Error(`Ya tiene una membresia activa. Vence el ${existing.end}`);
+        }
+        //aca customer probablemente sea el token del usuario loggeado por lo que habria que usar jwt para decodificar y sacar su email
+        const customer = user.email;
+        const charge=Number(price)*100;
+        const description= benefits + ' ' + duration;
+        const session=await stripe.checkout.sessions.create({
+            //aca el valor de customer email deberia ser, efectivamente el email una vez sacado del token
+            customer_email:customer,
+            line_items:[{
+                price_data:{
+                    currency: "USD",
+                    product_data:{
+                        name:name,
+                        description:description,
+                    },
+                    unit_amount:charge,
                 },
-                unit_amount:charge,
-            },
-            quantity: 1
-        }],
-        mode:'payment',
-        success_url: 'http://localhost:3000/clases',
-        cancel_url:'http://localhost:3000/sedes'
-    });
-    
-    res.status(200).json({redirect_url:session.url});
+                quantity: 1
+            }],
+            mode:'payment',
+            success_url: 'http://localhost:3000/clases',
+            cancel_url:'http://localhost:3000/sedes'
+        });
+        
+        res.status(200).json({redirect_url:session.url});
+    } catch (error) {
+        res.status(400).json({error:error.message});    
+    }
     
 });
 
