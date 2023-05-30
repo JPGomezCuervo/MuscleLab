a2df848cc641063ce80c86f663d66bdd2aa9e705
jpgomezcuervo <122913492+JPGomezCuervo@users.noreply.github.com>
2023-05-30 18:00:57 -0500

    Fix navbar, add login and logout, responsive details

--- client/src/components/DetailLesson/DetailLesson.jsx
+++ client/src/components/DetailLesson/DetailLesson.jsx
@@ -133,8 +133,8 @@ const DetailLesson = () => {
             {
               token?(
                 <div className={style.card}>
+                  <h2>Calificacion:  {lesson?.name} </h2>
                 <div className={style.rating}>
-                  
                   <input value={5} name="stars" id="star-1" type="radio" onChange={(e) => handleInputChange(e,lesson.id)} />
                   <label for="star-1">
                     <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z" pathLength="360"></path></svg>
@@ -155,8 +155,6 @@ const DetailLesson = () => {
                   <label for="star-5">
                     <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z" pathLength="360"></path></svg>
                   </label>
-                  <h2>{lesson?.name}</h2>
-                  <h2>Calificacion:  </h2>
                 </div>
 
                 <label htmlFor="">Comentario de la clase</label>
@@ -201,11 +199,40 @@ const DetailLesson = () => {
                   className={style.imagenDeporte}
                 />
 
+              {
+              token?(
+                <div className={style.card}>
+                  <h2>Calificacion:  {lesson?.name} </h2>
+                <div className={style.rating}>
+                  <input value={5} name="stars" id="star-1" type="radio" onChange={(e) => handleInputChange(e,lesson.id)} />
+                  <label for="star-1">
+                    <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z" pathLength="360"></path></svg>
+                  </label>
+                  <input value={4} name="stars" id="star-2" type="radio" onChange={(e) => handleInputChange(e,lesson.id)} />
+                  <label for="star-2">
+                    <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z" pathLength="360"></path></svg>
+                  </label>
+                  <input value={3} name="stars" id="star-3" type="radio" onChange={(e) => handleInputChange(e,lesson.id)} />
+                  <label for="star-3">
+                    <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z" pathLength="360"></path></svg>
+                  </label>
+                  <input value={2} name="stars" id="star-4" type="radio" onChange={(e) => handleInputChange(e,lesson.id)} />
+                  <label for="star-4">
+                    <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z" pathLength="360"></path></svg>
+                  </label>
+                  <input value={1} name="stars" id="star-5" type="radio" onChange={(e) => handleInputChange(e,lesson.id)} />
+                  <label for="star-5">
+                    <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z" pathLength="360"></path></svg>
+                  </label>
+                </div>
 
+                <label htmlFor="">Comentario de la clase</label>
+                <input type="text" value={input.description} placeholder='Descripcion' onChange={(e) => handleInputChange(e,lesson.id)} name="description" className={style.text} />
 
-
-
-
+                <button onClick={handlerReview} className={style.button}>Enviar</button>
+              </div>
+              ):(<div></div>)
+            }
                 </div>
 
               </div>
