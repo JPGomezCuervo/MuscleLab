d0c0d17b5c79e48a901bc04c3e25c860880128e5
lucasG505 <lucas.mariano.g@gmail.com>
2023-05-31 14:56:50 -0300

    import con require

--- back/src/Controllers/Payments/mailer.js
+++ back/src/Controllers/Payments/mailer.js
@@ -1,4 +1,4 @@
-const {nodemailer} = require('nodemailer')
+const nodemailer = require('nodemailer')
 
 // create reusable transporter object using the default SMTP transport
 const transporter = nodemailer.createTransport({
