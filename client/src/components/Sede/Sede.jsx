3a48b49c64df57ea626f14c8ede79fceff0c5e8d
jpgomezcuervo <122913492+JPGomezCuervo@users.noreply.github.com>
2023-06-27 18:35:05 +0700

    Re-factor sedes component

--- client/src/components/Sede/Sede.jsx
+++ client/src/components/Sede/Sede.jsx
@@ -1,39 +1,30 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import style from "./Sede.module.css";
-import fachada1 from "../../assets/images/sedes/fachada-1-gym.jpg";
-import fachada2 from "../../assets/images/sedes/fachada-2-gym.jpg";
 
 const Sede = ({ sede, index }) => {
-  const { name, location, scheduleDays, scheduleHours, id, image} = sede;
+  const { name, id, image} = sede;
   const card = index % 2 === 0 ? "Right" : "Left";
 
   return (
     <div className={style.Card}>
       {card === "Right" ? (
-        <div className={style.contenedor}>
+        <>
           <div className={style.ImageContainer}>
-            <img src={image} alt="fachada 1" />
+            <img src={image} alt="" />
           </div>
 
           <div className={style.InfoContainer}>
             <h2>{name}</h2>
-            {/* <p>Dirección: {location}</p>
-            <p>Días de atención: {scheduleDays}</p>
-            <p>Horario: {scheduleHours}</p> */}
-
             <Link to={`/sedes/detalles/${id}`}>
               <button>Detalles aquí</button>
             </Link>
           </div>
-        </div>
+        </>
       ) : (
-        <div className={style.content}>
+        <>
           <div className={style.InfoContainer}>
             <h2>{name}</h2>
-            {/* <p>{location}</p>
-            <p>{scheduleDays}</p>
-            <p>{scheduleHours}</p> */}
 
             <Link to={`/sedes/detalles/${id}`}>
               <button>Detalles aquí</button>
@@ -43,7 +34,7 @@ const Sede = ({ sede, index }) => {
           <div className={style.ImageContainer}>
             <img src={image} alt="fachada 2" />
           </div>
-        </div>
+        </>
       )}
     </div>
   );
